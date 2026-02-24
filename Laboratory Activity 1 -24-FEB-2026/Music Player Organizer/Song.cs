7108332c71556a4cc3416d63974f6098f1d12ef9
IvanDesierto <ivan.c.desierto@gmail.com>
2026-02-24 09:41:15 +0800

    Create Song.cs

---
+++ Laboratory Activity 1 -24-FEB-2026/Music Player Organizer/Song.cs
@@ -0,0 +1,92 @@
+class Song
+{
+    public string title;
+    public string artist;
+    public double duration;
+
+    public Song()
+    {
+        this.title = "Unknown";
+        this.artist = "Unknown";
+        this.duration = 0.00;
+    }
+
+    public Song(string title, string artist, double duration)
+    {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+    }
+
+    public Song(string title, string artist)
+    {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public void DisplaySong()
+    {
+        Console.WriteLine($"{title,-20} {artist,-20} {duration:F2}");
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Songs to add: ");
+        int SongCount = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        Song[] playlist = new Song[SongCount];
+
+        for (int i = 0; i < SongCount; i++)
+        {
+            Console.WriteLine($"Song #{i + 1}");
+            Console.Write("Title: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Artist: ");
+            string artist = Console.ReadLine();
+
+            Console.Write("Duration (minutes): ");
+            string durationInput = Console.ReadLine();
+      
+            Console.WriteLine();
+
+
+            if (string.IsNullOrWhiteSpace(title) && string.IsNullOrWhiteSpace(artist) && string.IsNullOrWhiteSpace(durationInput))
+            {
+
+                playlist[i] = new Song();
+            }
+            else if (string.IsNullOrWhiteSpace(durationInput))
+            {
+
+                playlist[i] = new Song(title, artist);
+            }
+            else
+            {
+
+                double duration = double.Parse(durationInput);
+                playlist[i] = new Song(title, artist, duration);
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("=== || MY PLAYLIST || ===");
+        Console.WriteLine($"{"Title",-20} {"Artist",-20} {"Time"}");
+        Console.WriteLine(new string('-', 50));
+
+        double totalDuration = 0;
+
+
+        for (int i = 0; i < SongCount; i++)
+        {
+            playlist[i].DisplaySong();
+            totalDuration += playlist[i].duration;
+        }
+
+        double averageDuration = totalDuration / SongCount;
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine($"Total Duration: {totalDuration:F2} mins");
+        Console.WriteLine($"Average Duration: {averageDuration:F2} mins");
+    }
+}
